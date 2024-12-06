246bbd2c62f4d5a61d9b72d199141610fedd304a
sergiusd <sergiusd@ya.ru>
2024-12-06 15:44:19 +0300

    Deploy admin to stage.

--- deploy/deploy.mk
+++ deploy/deploy.mk
@@ -2,7 +2,7 @@ docker-build:
 	docker build -f deploy/Dockerfile -t lms-redbus:latest ./
 
 docker-build-admin:
-	docker build -f deploy/admin/Dockerfile -t lms-redbus-admin:latest ./
+	docker build -f deploy/admin/Dockerfile -t lms-redbus-admin:latest --build-arg apiHost=redbus:5006 ./
 
 docker-run-admin: docker-build-admin
 	docker run -d -p 8080:80 --name lms-redbus-admin lms-redbus-admin:latest
