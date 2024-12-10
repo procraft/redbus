379e7aa7694c7681f1601dbbbdeafc8f93685247
sergiusd <sergiusd@ya.ru>
2024-12-10 17:38:13 +0300

    Try to fix CORS.

--- internal/app/adminapi/auth.go
+++ internal/app/adminapi/auth.go
@@ -8,12 +8,14 @@ import (
 func AuthMiddleware(token string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			value := r.Header.Get("Authorization")
-			parts := strings.Split(value, " ")
-			if len(parts) != 2 || strings.ToLower(parts[0]) != "token" || parts[1] != token {
-				w.WriteHeader(http.StatusForbidden)
-				_, _ = w.Write([]byte("Wrong authenticate token"))
-				return
+			if r.Method != http.MethodOptions {
+				value := r.Header.Get("Authorization")
+				parts := strings.Split(value, " ")
+				if len(parts) != 2 || strings.ToLower(parts[0]) != "token" || parts[1] != token {
+					w.WriteHeader(http.StatusForbidden)
+					_, _ = w.Write([]byte("Wrong authenticate token"))
+					return
+				}
 			}
 
 			next.ServeHTTP(w, r)
